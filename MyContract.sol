aca3d2cad257279b45f609c1663d16a64a50f7da
Andre Dublin <81dublin@gmail.com>
2018-01-02 21:42:31 -0500

    Simple contract example

---
+++ MyContract.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.19;
+
+import "github.com/ethereum/solidity/std/mortal.sol";
+
+contract MyContract is mortal {
+    uint myVariable;
+    
+    function MyContract() public payable {
+        myVariable = 5;
+    }
+    
+    function setMyVariable(uint myNewVariable) public onlyowner {
+        myVariable = myNewVariable;
+    }
+ 
+    function getMyVariable() public constant returns(uint) {
+        return myVariable;
+    }
+    
+    function getMyContractBalance() public constant returns(uint) {
+        return this.balance;
+    }
+    
+    function () public payable {
+        
+    }
+}
